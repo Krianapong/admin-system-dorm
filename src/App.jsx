4215893230d86421ee8b3975a20e717bd405ad39
Krisanapong Jareunjanporn <150851052+Krianapong@users.noreply.github.com>
2024-01-15 21:06:19 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
 import News from "./page/admin/news/New";
 import Room from "./page/admin/room/Room";
@@ -20,27 +19,44 @@ import Paybill from "./page/admin/paybill/Paybill";
 import Notifybill from "./page/admin/notifybill/Notifybill";
 import Water from "./page/admin/water/Water";
 import Electricity from "./page/admin/electricity/Electricity";
+import { Box, CssBaseline } from "@mui/material";
 
 function App() {
   return (
     <>
-      <Router>
-        <SideBar />
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/calendar" element={<Calender />} />
-          <Route path="/personnel" element={<Personnel />} />
-          <Route path="/detail" element={<Detail />} />
-          <Route path="/rooms" element={<Room />} />
-          <Route path="/water" element={<Water />} />
-          <Route path="/electricity" element={<Electricity />} />
-          <Route path="/paybill" element={<Paybill />} />
-          <Route path="/notifybill" element={<Notifybill />} />
-
-        </Routes>
-      </Router>
+      <div
+        className="app"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CssBaseline />
+        <Router>
+          <SideBar />
+          <main className="content">
+            {/* <HeaderBar /> */}
+            <div className="content_body">
+              <Box m="20px">
+                <Routes>
+                  <Route path="/" element={<SignIn />} />
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/news" element={<News />} />
+                  <Route path="/calendar" element={<Calender />} />
+                  <Route path="/personnel" element={<Personnel />} />
+                  <Route path="/detail" element={<Detail />} />
+                  <Route path="/rooms" element={<Room />} />
+                  <Route path="/water" element={<Water />} />
+                  <Route path="/electricity" element={<Electricity />} />
+                  <Route path="/paybill" element={<Paybill />} />
+                  <Route path="/notifybill" element={<Notifybill />} />
+                </Routes>
+              </Box>
+            </div>
+          </main>
+        </Router>
+      </div>
     </>
   );
 }
